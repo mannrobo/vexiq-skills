c0c0a394b29d435eeb9cbcd44983e021dd3643af
Brendan McGuire <brendan@bren.app>
2020-02-25 00:19:27 -0500

    index

--- src/renderer/routes/Index.tsx
+++ src/renderer/routes/Index.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Typography, Button } from "antd";
+import { Link } from "react-router-dom";
+const { Title } = Typography;
 
 export default () => (
   <Row id="index">
-    <img src="../public/images/viq_button.png" />
-    <Col span={8} />
-    <Col span={16}>
-      <p>Hello World</p>
+    <Col span={6} />
+    <Col span={12} className="content">
+      <img src="../public/images/viq_button.png" />
+      <Title level={3}>South Carolina VEX IQ State Championship</Title>
+
+      <Button type="primary" size="large" block>
+        <Link to="/host">Host</Link>
+      </Button>
+
+      <Button size="large" block>
+        <Link to="/client">Client</Link>
+      </Button>
+
+      <Button size="large" block>
+        <Link to="/display">Display</Link>
+      </Button>
     </Col>
   </Row>
 );
