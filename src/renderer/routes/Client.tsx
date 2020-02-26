40f0ccfd7057e79593c650e6b115eaa75cb29301
Brendan McGuire <brendan@bren.app>
2020-02-26 18:05:32 -0500

    Client connection

--- src/renderer/routes/Client.tsx
+++ src/renderer/routes/Client.tsx
@@ -37,74 +37,102 @@ export default class ClientRoute extends React.Component {
 
   ws: WebSocket;
 
-  handleSubmit() {
+  handleSubmit = () => {
+    this.setState({
+      iq: {
+        connecting: true
+      }
+    });
     this.ws = new WebSocket(`ws://${this.state.iq.address}`);
 
     this.ws.addEventListener("open", () => {
+      console.log("connected");
       this.ws.send(
         JSON.stringify({
           action: "SET_NAME",
           name: this.state.iq.name
         })
       );
+
+      this.setState({
+        iq: {
+          connecting: false,
+          connected: false
+        }
+      });
+
+      window.location.hash = "#/client/connected";
     });
-  }
+
+    this.ws.addEventListener("error", error => {
+      this.setState({
+        error
+      });
+    });
+  };
 
   render() {
     return (
-      <Router basename="/host">
-        <Switch>
-          <Route path="/">
-            <p>Connect to VEX IQ SKILLS MANAGEMENT</p>
-            <Form layout="vertical" onSubmit={this.handleSubmit}>
-              {this.state.iq.error ? (
-                <Alert message={this.state.iq.error} type="error" />
-              ) : null}
-              <Form.Item>
-                <Input
-                  addonBefore={"ws://"}
-                  placeholder="Address"
-                  onInput={event =>
-                    this.setState({
-                      tm: {
-                        ...this.state.iq,
-                        address: (event.target as HTMLInputElement).value
-                      }
-                    })
-                  }
-                  value={this.state.iq.address}
-                />
-              </Form.Item>
-              <Form.Item>
-                <Input
-                  prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                  }
-                  placeholder="Your Client Name"
-                  onInput={event =>
-                    this.setState({
-                      tm: {
-                        ...this.state.iq,
-                        name: (event.target as HTMLInputElement).value
-                      }
-                    })
-                  }
-                  value={this.state.iq.name}
-                />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  type="primary"
-                  htmlType="submit"
-                  loading={this.state.iq.connecting}
-                >
-                  Connect
-                </Button>
-              </Form.Item>
-            </Form>
-          </Route>
-        </Switch>
-      </Router>
+      <Layout id="client">
+        <Row>
+          <Col span={this.state.iq.connected ? 1 : 6}></Col>
+          <Col span={12}>
+            <Router basename="/client">
+              <Switch>
+                <Route path="/connected">
+                  <p>Connected</p>
+                </Route>
+                <Route path="/">
+                  <p>Connect to VEX IQ Skills Host Server</p>
+                  <Form layout="vertical" onSubmit={this.handleSubmit}>
+                    {this.state.iq.error ? (
+                      <Alert message={this.state.iq.error} type="error" />
+                    ) : null}
+                    <Form.Item>
+                      <Input
+                        addonBefore={"ws://"}
+                        placeholder="Address"
+                        onInput={event =>
+                          this.setState({
+                            iq: {
+                              ...this.state.iq,
+                              address: (event.target as HTMLInputElement).value
+                            }
+                          })
+                        }
+                        value={this.state.iq.address}
+                      />
+                    </Form.Item>
+                    <Form.Item>
+                      <Input
+                        placeholder="Client Name"
+                        onInput={event =>
+                          this.setState({
+                            iq: {
+                              ...this.state.iq,
+                              name: (event.target as HTMLInputElement).value
+                            }
+                          })
+                        }
+                        value={this.state.iq.name}
+                      />
+                    </Form.Item>
+                    <Form.Item>
+                      <Button
+                        type="primary"
+                        htmlType="submit"
+                        loading={this.state.iq.connecting}
+                      >
+                        Connect
+                      </Button>
+                    </Form.Item>
+                  </Form>
+                </Route>
+              </Switch>
+            </Router>
+          </Col>
+        </Row>
+      </Layout>
     );
   }
 }
